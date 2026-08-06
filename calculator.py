8205b688cde973b984354bc9d75c6ad9cc6bf1fb
AbuShahma05 <abushahmasiddiqui880@gmail.com>
2026-08-06 20:28:09 +0530

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,12 @@
+operation = input("Choose operation (+, -, *, /): ")
+
+if operation == "+":
+   number = float(input("enter number"))
+   number2 = float(input("Enter Number"))
+   print(number + number2)
+
+if operation == "-":
+   num3 = float(input("enter number"))
+   num4 = float(input("enter number"))
+   print(num3 - num4)
+
